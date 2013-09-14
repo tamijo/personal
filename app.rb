02368b8224caf6fb7100af25fc6a9c8534f539bd
Tami Forrester <tforrest@mit.edu>
2013-09-14 14:32:54 -0400

    add coming soon for links

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
 get '/' do
-  erb :index
+	erb :index
+end
+get '/about' do
+	"Coming soon!"
+end
+get '/projects' do
+	"Comming soon!"
+end
+get '/resume' do
+	"Coming soon!"
+end
+not_found do
+  'Whoops......this page does not exist!'
 end
