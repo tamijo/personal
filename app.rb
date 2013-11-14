6708dbf8cb82a3b28b082441a9819c0d73211cd8
Tami Forrester <tforrest@mit.edu>
2013-11-13 22:35:45 -0500

    added projects

--- app.rb
+++ app.rb
@@ -4,10 +4,13 @@ require 'sass'
 PROJECTS = [
 	{
 	 	"title" => "Call to Action",
-	 	"subtitle" => "User Interface",
-	 	"description" => "derp",
-	 	"url" => "derp",
-	 	"img" => "derp"
+	 	"subtitle" => "MIT's Center for Civic Media / January-June 2013",
+	 	"description" => "An interface made in HTML, CSS, and JavaScript for 
+	 	<a href='http://calltoaction.mit.edu'>Call To Action</a>, an open-source web platform 
+	 	for creating telephone-based services being developed at 
+	 	<a href='http://civic.mit.edu/'>MIT's Center for Civic Media</a>.",
+	 	"url" => "https://github.com/rodrigodavies/call2action/",
+	 	"img" => "/img/calltoaction.jpg"
 		},
 	{
 		"title" => "StageIt",
@@ -15,7 +18,7 @@ PROJECTS = [
 		"description" => "A small application created with HTML, CSS, JavaScript using Parse
 		for 6.813, the User Interface Design and Implementation class at MIT",
 		"url" => "https://github.com/6813-stageIt/stageIt",
-		"img" => "not quite there yet"
+		"img" => "/img/stageit.jpg"
 	},
 	{
 		"title" => "Hunters and Zombies",
@@ -23,7 +26,7 @@ PROJECTS = [
 		"description" => "A geolocation game made for Android phones in 21W.789, the Communicating
 		with Mobile Technology class.",
 		"url" => "https://github.com/TeamGamey/HuntersAndZombiesDemo",
-		"img" => "not quite there yet"
+		"img" => "/img/huntersandzombies.png"
 	}
 ]
 
